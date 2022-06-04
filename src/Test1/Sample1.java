4234b0c48910ac8db86e7ee5ea572b218755780a
Hp <Hp@DESKTOP-VL5K6FE>
2022-06-03 21:59:34 -0700

    [Sonalee] 1ST Commit

---
+++ src/Test1/Sample1.java
@@ -0,0 +1,15 @@
+package Test1;
+
+public class Sample1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi");
+		
+		System.out.println("Hello");
+		
+		System.out.println("Good Morning");
+		
+	}
+
+}
